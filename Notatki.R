329319b3269860e323973165b9f455a0b9cefee5
Mateusz Pawlaczyk <168810098+automat9@users.noreply.github.com>
2024-08-29 06:25:46 +0100

    Update Notatki.R

--- Notatki.R
+++ Notatki.R
@@ -1,1 +1,4 @@
 x <- 5 # this is how you assign numbers to variables
+power_rangers_seasons <- read_csv("C:/Users/10/Desktop/power_rangers_seasons.csv") # example of how to assign a dataset to a variable
+View(power_rangers_seasons) # view your dataset, CASE SENSITIVE
+mean(power_rangers_seasons$season_num) # Matt, you'll never believe what this does
