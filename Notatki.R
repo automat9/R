11f34baeb687c269db05cd2c0d6acb1d70d89d68
Mateusz Pawlaczyk <168810098+automat9@users.noreply.github.com>
2024-09-02 07:18:30 +0100

    Update Notatki.R

--- Notatki.R
+++ Notatki.R
@@ -1,3 +1,6 @@
+===============================================================================================================================================================================================================
+# Absolute basics
+
 x <- 5 # this is how you assign numbers to variables
 power_rangers_seasons <- read_csv("C:/Users/10/Desktop/power_rangers_seasons.csv") # example of how to assign a dataset to a variable
 View(power_rangers_seasons) # view your dataset, CASE SENSITIVE
@@ -13,3 +16,23 @@ ggplot(power_rangers_seasons, aes(x = season_num, y = number_of_episodes)) +
   geom_line(size = 2, color = "red") +
   scale_x_continuous(breaks = 1:28) +
   labs(x = 'Season Number', y = 'Number of Episodes')
+
+===============================================================================================================================================================================================================
+# Basic Statistics
+library(tidyverse)
+data() # shows all datasets available IN THE CURRENTLY LOADED LIBRARY (in this case tidyverse)
+data(package = .packages(all.available = TRUE)) # shows all available datasets in all your downloaded libraries, whether they're loaded or not
+
+# Get an overview of the dataset (descriptives sorted by column)
+summary(USPersonalExpenditure) # USPers... is the table name
+
+# Obtaining mean from a table
+food_tobacco <-USPersonalExpenditure['Food and Tobacco',] # row = Food and Tobacco
+mean_food_tobacco <- mean(as.numeric(food_tobacco))
+print(mean_food_tobacco)
+
+# Obtaining mean from a vector
+food_tobacco <- c(22.200, 44.500, 59.60, 73.2, 86.80) # create a vector and name it
+mean_food_tobacco <- mean(food_tobacco)
+print(mean_food_tobacco)
+
